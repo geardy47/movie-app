4bd4efe48b16f95efebbaaeba9df1eb8c9765de3
geardy47 <muhammadgenta14@gmail.com>
2021-02-08 01:01:06 +0700

    Add search function

--- src/App.js
+++ src/App.js
@@ -4,22 +4,62 @@ import Movie from './compenents/Movies';
 
 const API_KEY = '&apikey=e1cb94d0';
 const FEATURED_MOVIES = 'http://www.omdbapi.com/?s=man';
+const SEARCH_MOVIES = 'http://www.omdbapi.com/?s=';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    fetch(FEATURED_MOVIES + API_KEY)
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data.Search);
         setMovies(data.Search);
       });
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_MOVIES + API_KEY);
   }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_MOVIES + searchTerm + API_KEY);
+
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div className='App'>
-      {movies.length > 0 &&
-        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)}
+      <header>
+        <h3>Movie App</h3>
+
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className='search'
+            type='search'
+            placeholder='Search'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className='movie-container'>
+        {movies !== undefined ? (
+          movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+        ) : (
+          <p>Data tidak ditemukan</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/compenents/Movies.js
+++ src/compenents/Movies.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Movie = ({ Title, Poster, Year, imdbID }) => {
   return (
     <div className='movie'>
-      <img src={Poster} alt={Title}></img>
+      <img
+        src={
+          Poster !== 'N/A'
+            ? Poster
+            : 'https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+        }
+        alt={Title}
+      ></img>
       <div className='movie-name'>
         <h3>
           {Title} ({Year})
